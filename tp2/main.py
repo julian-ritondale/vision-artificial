688d13f928ad7511f678167ba681ffc4d128f7c3
Ignacio Nicolás Correa Rodríguez <incorrearodriguez@mail.austral.edu.ar>
2026-08-22 00:34:53 -0300

    Implemented model into main file

--- tp2/main.py
+++ tp2/main.py
@@ -1,3 +1,5 @@
+from utils.trainer import train_decision_tree
 from utils.dataset_generator import generate_hu_moments_file
 
 generate_hu_moments_file()
+train_decision_tree(csv_path='dataset/tetris-hu-moments.csv', model_output_path='model/decision_tree_model.joblib', plot_output_path='model/decision_tree_plot.png')
